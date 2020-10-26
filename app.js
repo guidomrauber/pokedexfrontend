348c9a166d0550a455e83c86053a9d99e292c078
guidorauber <guidomrauber@gmail.com>
2020-10-26 13:15:10 +0000

    barras nav footer-

--- app.js
+++ app.js
@@ -9,21 +9,18 @@ const retrievePokemon = async () => {
             console.log(rawData.data);
             displayImage(rawData.data.sprites.front_default);
         } catch (error) {
+            // Si hay error ejecuto la funcion de error
+            showError()
             console.error(error)
         }
     }
 
 }
-	console.timeEnd('callPokemon');
-
-const showStats = stats => {
-
-}
 
 const displayImage = url => {
-    // crear nodo tipo img
-    const imgElement = document.createElement('img');
-    // agregar atributos
+    // Selecciono el elemento de la image y le seteo el valor al src para que lo dibuje, asi evito que cree muchas imagenes
+    let imgElement = document.getElementById("img")
+    imgElement.style.display = "flex";
     imgElement.setAttribute('src', url);
     imgElement.setAttribute('width', '200');
     imgElement.setAttribute('height', '200');
@@ -33,6 +30,32 @@ const displayImage = url => {
     divImg.appendChild(imgElement);
 }
 
+
+// Muestro  el mesaje que no hay resultado y oculto la imagen por si tiene algo y despues de 3 segundo limpio el mensaje
+const showError = () => {
+    // selecciono la image
+    let imgElement = document.getElementById("img")
+    // Le pongo display none para ocultar
+    imgElement.style.display = "none";
+    // Creo el mensaje de error
+    document.getElementById("msj").innerText = "No se encuentra el resultado"
+    // Despues de tres segundo borro el mensaje
+    setTimeout(() => {
+        document.getElementById("msj").innerText = ""
+    }, 3000);
+}
+
 window.onload = () => {
     document.getElementById('get-pokemons').addEventListener('click', retrievePokemon);
+    /*
+    1- Selecciono el input no el boton
+    2- Con el addListener keyup toma el valor por cada tecla que apretamos
+    3- el keycode es el valor de la tecla , el enter es el valor trece
+    4- si apretan enter ejecuta la funcion para buscar el pokemon
+    */
+    document.getElementById('pokemon-name').addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+            retrievePokemon()
+        }
+    });
 }

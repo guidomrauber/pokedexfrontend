be0cb50ce439c53da8fd1a22b2eb6aeaa3e122e0
guidomrauber <49770318+guidomrauber@users.noreply.github.com>
2020-12-01 12:36:06 -0300

    creando pagina eliminar

--- js/listado2.js
+++ js/listado2.js
@@ -1,137 +1,127 @@
-const URL_POKEAPI = "https://localhost:3000/api/pokemon/";
-
-// Lista de pokemones consultados
-const requestedPokemons = [];
-
-document.getElementById('username').onkeyup = function() {
-
-}
-var order2 = 0;
-var name = "";
-var type = "";
-var habilidad = "";
-var sampleImage = "";
-
-
-function formData(event) {
-  cleanList();
-  event.preventDefault();
-  let username = document.getElementById('username').value;
-  displayInfo(username);
-  cleanForm();
-}
-
-function cleanForm() {
-  document.getElementById('username').value = '';
-}
-
-function cleanList() {
-  const list = document.getElementById('pokemon-list');
-  if (list.childNodes.length > 0) {
-    list.removeChild(list.childNodes[0])
-  }
-  const errorDiv = document.getElementById('error-message');
-  errorDiv.style.display = "none";
-}
-
-// muestra info específica de cada pokemon
-function displayInfo(data) {
-  getPokemonData(data);
-  
-}
-
-// obtiene los datos de pokemones
-function getPokemonData(name, index) {
-  document.getElementById('error-message').style.display = "none";
-  if (!requestedPokemons.includes(name)) {
-    axios.get(URL_POKEAPI + name).then(resp => {
-      requestedPokemons.push(resp.data.name);
-      displayPokemonInfo(resp.data, index);
-    }).catch(error => {
-      console.error(error);
-      showErrorMessage(error);
-    });
-  }
-}
-
-// despliega el mensaje de error
-function showErrorMessage(error) {
-  const errorDiv = document.getElementById('error-message');
-  errorDiv.style.display = "inline";
-  const spanError = document.getElementById('error-content');
-  spanError.innerHTML = error.message;
-}
-
-// Crear elementos de lista y muestra el nombre junto a la imagen
-function displayPokemonInfo(data, index = 0) {
-  const tableList = document.getElementById('pokemon-list').getElementsByTagName('tbody')[0];
-  const row = tableList.insertRow();
-  
-  const cellID = row.insertCell();
-  const cellName = row.insertCell();
-  const cellImage = row.insertCell();
-  const cellAbilities = row.insertCell();
-  const cellTypes = row.insertCell();
-
-  const abilitiesList = document.createElement('ul');
-  order2 = cellID;
-  name = cellName;
-  type =  cellTypes;
-  sampleImage = cellImage;
-  habilidad = cellAbilities;
-  data.abilities.reverse().map(attack => {
-    const liAbility = document.createElement('li');
-    liAbility.innerHTML = attack.ability.name.firstToUpperCase();
-    abilitiesList.appendChild(liAbility);
-  });
-  cellAbilities.append(abilitiesList);
-
-  const typesList = document.createElement('ul');
-  data.types.map(types => {
-    const typeLi = document.createElement('li');
-    typeLi.innerHTML = types.type.name.firstToUpperCase();
-    typesList.appendChild(typeLi);
-  });
-  cellTypes.append(typesList);
-
-  const img = document.createElement('img');
-  img.src = data.sprites.front_shiny;
-  img.title = data.name.firstToUpperCase();
-  cellImage.append(img);
-  cellID.innerHTML = data.id;
-  cellName.innerHTML = data.name.firstToUpperCase();
-  let order = data.id;
-}
-
-let i = 1;
-
-function getFirstfive() {
-  const ii = i + 5;
-  for (i; i < ii; i++) {
-    getPokemonData(i, i);
-  }
-}
-function eatFood() {
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({"order":"8","name":"joya","sampleImage":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/5.png","type":"Fire","habilidad":"Fir "});
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
+const poke_container = document.getElementById('poke_container');
+const pokemons_num = 5; // 5 pokemon in 1st generation
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
 };
 
-fetch("http://localhost:3000/api/pokemon/", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+const main_types = Object.keys(colors);
+
+
+//const fetch_pokemon = async() =>{
+//   for(let i=1; i<=pokemons_num; i++){
+//     await get_pokemon(i);
+//  }
 	
-	}
-     
-  
-String.prototype.firstToUpperCase = function firstToUpperCase() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+//}
+
+
+// this functions calls the pokemon API 
+var id =1;
+//do {
+const requestURL1 = "http://localhost:8080/api/pokedexs/1";
+//const requestURL = "http://localhost:8080/api/pokedexs/${i}";
+const request1 = new XMLHttpRequest();
+request1.open('GET', requestURL1);
+request1.responseType = 'json';
+request1.send();
+request1.onload = function() {
+  const pokemon = request1.response;
+  create_pokemon_card(pokemon);
 }
+//i++;
+//}while(i<=5);
+
+const requestURL2 = "http://localhost:8080/api/pokedexs/2";
+const request2 = new XMLHttpRequest();
+request2.open('GET', requestURL2);
+request2.responseType = 'json';
+request2.send();
+request2.onload = function() {
+  const pokemon2 = request2.response;
+  create_pokemon_card(pokemon2);
+}
+const requestURL3 = "http://localhost:8080/api/pokedexs/3";
+const request3 = new XMLHttpRequest();
+request3.open('GET', requestURL3);
+request3.responseType = 'json';
+request3.send();
+request3.onload = function() {
+  const pokemon3 = request3.response;
+  create_pokemon_card(pokemon3);
+}
+const requestURL4 = "http://localhost:8080/api/pokedexs/4";
+const request4 = new XMLHttpRequest();
+request4.open('GET', requestURL4);
+request4.responseType = 'json';
+request4.send();
+request4.onload = function() {
+  const pokemon4 = request4.response;
+  create_pokemon_card(pokemon4);
+}
+const requestURL5 = "http://localhost:8080/api/pokedexs/5";
+const request5 = new XMLHttpRequest();
+request5.open('GET', requestURL5);
+request5.responseType = 'json';
+request5.send();
+request5.onload = function() {
+  const pokemon5 = request5.response;
+  create_pokemon_card(pokemon5);
+}
+//var requestOptions = {
+//  method: 'GET',
+//  redirect: 'follow'
+//};
+
+//fetch("http://localhost:8080/api/pokedexs/1", requestOptions)
+ // .then(response => response.json())
+ // .then(result => console.log(result))
+ // .catch(error => console.log('error', error));
+
+
+
+function create_pokemon_card(Pokemon){
+    const pokemon_el = document.createElement('div');
+    pokemon_el.classList.add('pokemon');
+//const poke_type = pokemon.type.map(el => el.type.name);
+const  pokemon = Pokemon;
+
+	
+	const type = pokemon.type;
+	const order = pokemon.order;
+    const name = pokemon.name;
+	const id = pokemon.id;
+    const color = colors[type];
+
+    pokemon_el.style.backgroundColor = color;
+
+    const pokemon_inner_html = `
+        <div class="img_container"> 
+            <img src="https://pokeres.bastionbot.org/images/pokemon/${order}.png" />
+        </div>
+        <div class="info">
+            
+            <h3 class = "id">ID ${id}</h3>
+              
+        </div>
+        
+    `;
+
+    pokemon_el.innerHTML = pokemon_inner_html;
+
+    poke_container.appendChild(pokemon_el);
+}
+
+//fetch_pokemon();
